77010f83a594201e6b334fc39f0211ba131728fd
Daren Sipes <daren@teamwilletts.com>
2020-03-24 10:58:13 -0400

    Cleaning up JsonType
    Looks like cake added the JsonType. We had been using our own verison for several years.

--- src/Database/Type/JsonArrayType.php
+++ src/Database/Type/JsonArrayType.php
@@ -1,5 +1,10 @@
 <?php
+declare(strict_types=1);
+
 namespace ButterCream\Database\Type;
+
+use Cake\Database\DriverInterface;
+use Cake\Database\JsonType;
 
 /**
  * Provides behavior for the JSON type
@@ -8,8 +13,36 @@ class JsonArrayType extends JsonType
 {
 
     /**
-     * Do you want this to be an object or array - we want the array!
-     * @var boolean
+     * Convert string values to PHP arrays.
+     *
+     * @param mixed $value The value to convert.
+     * @param \Cake\Database\DriverInterface $driver The driver instance to convert with.
+     * @return string|array|null
      */
-    public $array = true;
+    public function toPHP($value, DriverInterface $driver)
+    {
+        if (!is_string($value)) {
+            return null;
+        }
+
+        return json_decode($value, false);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return array
+     */
+    public function manyToPHP(array $values, array $fields, DriverInterface $driver): array
+    {
+        foreach ($fields as $field) {
+            if (!isset($values[$field])) {
+                continue;
+            }
+
+            $values[$field] = json_decode($values[$field], false);
+        }
+
+        return $values;
+    }
 }
