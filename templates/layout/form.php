840d5726373b29a49181b321966bf4eb023eaee1
Daren Sipes <daren@teamwilletts.com>
2020-06-18 16:04:23 -0400

    Opps. Some testing slipped in

--- templates/layout/form.php
+++ templates/layout/form.php
@@ -1,5 +1,5 @@
 <?php $this->set('sessionMonitor', true); ?>
 <?php $this->layout == 'ajax' ? null : $this->extend('ButterCream./layout/common'); ?>
-<div class="row test">
+<div class="row">
     <?= $this->fetch('content'); ?>
 </div>
