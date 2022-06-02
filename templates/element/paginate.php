75980d4fcae0330e7b7f33c7cc3935edcb2ca3c0
Daren Sipes <daren@teamwilletts.com>
2022-06-02 09:31:09 -0400

    Paginator Fix for the default

--- templates/element/paginate.php
+++ templates/element/paginate.php
@@ -1,4 +1,5 @@
-<?php $paginatorOptions = $this->get('paginator.options', [
+<?php
+$paginatorOptions = $this->get('paginator.options', [
     'limit' => [
         'show' => true,
         'options' => [
@@ -10,7 +11,8 @@
         ],
         'default' => 20,
     ],
-]); ?>
+]);
+?>
 
 <div class="row">
     <div class="col-auto mr-auto">
@@ -22,7 +24,7 @@
                     'data-url' => Cake\Routing\Router::url(['?' => $this->getRequest()->getQuery()]),
                     'data-update' => $this->fetch('ajax_update_element'),
                     'options' => !empty($paginatorOptions['limit']['options']) && is_array($paginatorOptions['limit']['options']) ? $paginatorOptions['limit']['options'] : [20 => 20, 40 => 40, 60 => 60, 80 => 80, 100 => 100],
-                    'default' => $this->getRequest()->getQuery('limit') ? $this->getRequest()->getQuery('limit') : $paginatorOptions['limit']['options'][20] ?? 20,
+                    'default' => $this->getRequest()->getQuery('limit') ? $this->getRequest()->getQuery('limit') : $paginatorOptions['limit']['default'] ?? 20,
                     'templates' => [
                         'select' => '<select class="input-xs ' . ($this->layout == 'ajax' ? 'ajax-set-pagination-limit' : 'set-pagination-limit') . '"{{attrs}}>{{content}}</select>',
                         'inputContainer' => 'Show: {{content}}',
