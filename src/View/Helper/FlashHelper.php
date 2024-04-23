473f7834354eae8edb4dcd0c3ffe3604bdb1a92b
Daren Sipes <daren.sipes@willettstech.com>
2024-04-23 13:57:34 -0400

    Updating the iconmap

--- src/View/Helper/FlashHelper.php
+++ src/View/Helper/FlashHelper.php
@@ -10,4 +10,27 @@ use BootstrapUI\View\Helper\FlashHelper as Helper;
  */
 class FlashHelper extends Helper
 {
+    /**
+     * Default config
+     *
+     * - class: List of classes to be applied to the div containing message
+     * - attributes: Additional attributes for the div containing message
+     * - icon: A boolean defining whether to use icons, or a string holding an icon name or HTML.
+     * - iconMap: A map of flash element names and icon definitions.
+     *
+     * @var array<string, mixed>
+     */
+    protected array $_defaultConfig = [
+        'class' => ['alert', 'alert-dismissible', 'fade', 'show', 'd-flex', 'align-items-center'],
+        'attributes' => ['role' => 'alert'],
+        'icon' => true,
+        'iconMap' => [
+            'default' => 'info-circle',
+            'success' => 'check-circle',
+            'error' => 'exclamation-triangle',
+            'info' => 'info-circle',
+            'warning' => 'exclamation-triangle',
+        ],
+        'element' => 'BootstrapUI.flash/default',
+    ];
 }
